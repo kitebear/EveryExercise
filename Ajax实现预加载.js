a99f98d340a02ad951cbabf15d31ed6951faaa19
dhxie <xie5721072@163.com>
2016-03-15 11:51:39 +0800

    1

---
+++ Ajax实现预加载.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    setTimeout(function() {
+        // XHR to request a JS and a CSS
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://domain.tld/preload.js');
+        xhr.send('');
+        xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://domain.tld/preload.css');
+        xhr.send('');
+        // preload image
+        new Image().src = "http://domain.tld/preload.png";
+    }, 1000);
+};
